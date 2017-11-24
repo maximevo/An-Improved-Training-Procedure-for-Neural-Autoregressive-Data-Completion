aaaf4d5d46a270f0fe1e97bbfcb164096c04f9d2
Maxime Voisin <maximev@stanford.edu>
2017-11-24 07:26:08 -0800

    Update launch.py

--- exp_binary_dataset/launch.py
+++ exp_binary_dataset/launch.py
@@ -4,25 +4,13 @@ Keywords can control: small dataset or large dataset, scenario 1,2,3 or 4, model
 """
 
 import sys, os
-
-my_path = os.path.dirname( os.path.abspath(__file__) )
-root = os.path.dirname(os.path.dirname( os.path.abspath(__file__) ))
-
-print('my_path',my_path)
-print('root',root)
-
-
 if not (my_path in sys.path):
     sys.path.append(my_path)
 if not (root in sys.path):
     sys.path.append(root)
-    
 from model import Model
-
 from solver import Solver
-
 from config import Config
-
 import copy
 import sys
 import os
@@ -36,63 +24,24 @@ dataset_dna = 'dna'
 dataset_mushrooms = 'mushrooms'
 dataset_nips = 'nips'
 dataset_connect4 = 'connect4'
-# ======== EXPERIMENTS TO LAUNCH =============
 
+# ======== EXPERIMENTS TO LAUNCH =============
 batch_size = 128
 initial_learning_rate_1 = 0.1
 initial_learning_rate_2 = 0.05
-initial_learning_rate_3 = 0.01
-initial_learning_rate_4 = 0.005
-initial_learning_rate_5 = 0.001
-initial_learning_rate_6 = 0.0005
-initial_learning_rate_7 = 0.0001
-initial_learning_rate_8 = 0.00005
-initial_learning_rate_9 = 0.00001
-initial_learning_rate_10 = 0.000005
-initial_learning_rate_11 = 0.000001
-
 decay_steps = 2000
 decay_rate = 0.9
 gradient_clip_norm = 9
 early_stop = 20
-
-
-
-
-
 n_epochs = 2000
-
-
-
-
-
 
 config_1 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_1,\
                        decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
 config_2 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_2,\
                        decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_3 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_3,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_4 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_4,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_5 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_5,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_6 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_6,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_7 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_7,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_8 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_8,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_9 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_9,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_10 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_10,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
-config_11 = Config(batch_size=batch_size,n_epochs=n_epochs,initial_learning_rate=initial_learning_rate_11,\
-                       decay_steps=decay_steps,decay_rate=decay_rate,gradient_clip_norm=gradient_clip_norm,early_stop=early_stop)
 
-
-#config_list = [config_1,config_2,config_3,config_4,config_5,config_6,config_7,config_8,config_9,config_10,config_11]
-config_list = [config_4,config_5,config_6]
+#config_list = [config_1,config_2]
+config_list = [config_1,config_2]
 
 #size_datasets = ['small','large']
 size_datasets = ['small','large']
@@ -158,8 +107,7 @@ def launch_one_experiment(model,dataset,config, my_solver, my_scenario, my_model
     print('train_X.shape',train_X.shape)
     print('valid_X.shape',valid_X.shape)
     print('test_X.shape',test_X.shape)
-
-
+    
     # Instantiate config object
     config.timeslice_size = train_X.shape[1]
 
